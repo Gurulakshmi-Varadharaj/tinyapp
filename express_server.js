e4786f321a487e694c80f97eb138ffbe8a70943a
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-09-24 02:05:22 +0000

    Add condition to control edit after logged out

--- express_server.js
+++ express_server.js
@@ -104,9 +104,14 @@ app.get("/u/:shortURL", (req, res) => {
 //Using Template Engine to pass data from frontend to backend and vice versa
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = `${req.params.shortURL}`;
-  if (urlDatabase[shortURL] && urlDatabase[shortURL]['userID'] === currentUser) {
-    let templateVars = { user: users[currentUser], shortURL, longURL: urlDatabase[shortURL]['longURL'] };
-    return res.render('urls_show', templateVars);
+  if (req.session.userId != null) {
+    if (urlDatabase[shortURL] && urlDatabase[shortURL]['userID'] === currentUser) {
+      let templateVars = { user: users[currentUser], shortURL, longURL: urlDatabase[shortURL]['longURL'] };
+      return res.render('urls_show', templateVars);
+    } else {
+      res.statusCode = 404;
+      return res.send(`${res.statusCode}: Page not found`);
+    }
   } else {
     res.statusCode = 404;
     return res.send(`${res.statusCode}: Page not found`);
